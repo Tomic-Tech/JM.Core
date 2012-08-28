56a448aaefd7767913f6ef8579d372c449b94467
Rocky <wesharer@gmail.com>
2012-08-28 17:33:13 +0800

    Add TroubleCode Model

---
+++ JM/Core/TroubleCode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JM.Core
+{
+    public class TroubleCode : Notifier
+    {
+        private string code;
+        private string content;
+
+        public TroubleCode()
+        {
+            code = null;
+            content = null;
+        }
+
+        public TroubleCode(string code, string content)
+        {
+            this.code = code;
+            this.content = content;
+        }
+
+        public string Code
+        {
+            get { return code; }
+            set 
+            { 
+                code = value;
+                OnPropertyChanged("Code");
+            }
+        }
+
+        public string Content
+        {
+            get { return content; }
+            set
+            {
+                content = value;
+                OnPropertyChanged("Content");
+            }
+        }
+    }
+}
